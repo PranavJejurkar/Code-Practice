9954f43e3de790ae358a6d6979806c94d0ff853c
Pranav Jejurkar <95302595+PranavJejurkar@users.noreply.github.com>
2023-07-02 14:56:32 +0530

    Add files via upload

---
+++ StringReplace.java
@@ -0,0 +1,11 @@
+class StringReplace{
+
+                     public static void main(String args[]){
+
+                     String str = "hello my name is Swapnil";
+                     String str1 = "Pranav";
+                     System.out.println(str.replace("Swapnil",str1));
+                     
+
+                     } 
+}
